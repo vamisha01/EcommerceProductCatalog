96f064c2a40f8c07e929a0cc578cbb5ff3110bf4
vamisha01 <amishaupadhyay02@gmail.com>
2025-06-14 14:50:00 +0530

    Create app.js

---
+++ frontend/src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProductCatalog from './components/ProductCatalog';
+
+function App() {
+  return (
+    <div>
+      <h1>E-Commerce Product Catalog</h1>
+      <ProductCatalog />
+    </div>
+  );
+}
+
+export default App;
